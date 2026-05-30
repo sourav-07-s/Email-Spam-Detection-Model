8ec719b4533f4a26112b5e25c5b61732952a0ca5
sourav-07-s <149261984+sourav-07-s@users.noreply.github.com>
2026-05-30 20:24:29 +0530

    sucessfully test the code

--- model_Tranning/test_model.py
+++ model_Tranning/test_model.py
@@ -1,33 +1,27 @@
 import joblib
 
 model = joblib.load("spam_model.pkl")
-
 vectorizer = joblib.load("vectorizer.pkl")
+
+print("Spam Detection System")
+print("Type 'exit' to quit\n")
 
 while True:
 
-    email = input("\nEnter Email: ")
+    email = input("Enter Email: ")
 
-    vector = vectorizer.transform(
-        [email]
-    )
+    # Exit condition
+    if email.lower() == "exit":
+        print("Exiting Spam Detection System...")
+        break
 
-    prediction = model.predict(
-        vector
-    )[0]
+    vector = vectorizer.transform([email])
 
-    probability = model.predict_proba(
-        vector
-    )[0][1]
+    prediction = model.predict(vector)[0]
+
+    probability = model.predict_proba(vector)[0][1]
 
     if prediction == 1:
-
-        print(
-            f"🚨 Spam ({probability*100:.2f}%)"
-        )
-
+        print(f"🚨 Spam ({probability*100:.2f}%)")
     else:
-
-        print(
-            f"✅ Not Spam ({(1-probability)*100:.2f}%)"
-        )
+        print(f"✅ Not Spam ({(1-probability)*100:.2f}%)")
